f25428e468d2bfb9a2eafc7e590e7923dc1c8026
Станислав Хозяенок <st.hoz.kem@gmail.com>
2024-10-01 13:10:11 +0700

    Добавлена логика кнопки

--- app/src/main/java/com/example/nestedlayouts/MainActivity.kt
+++ app/src/main/java/com/example/nestedlayouts/MainActivity.kt
@@ -9,13 +9,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private var currentValue = 1
-    private lateinit var textViews: List<TextView>
+    private var currentValue = 0
+
+    //    private lateinit var textViews: List<TextView>
+    private var textViews = Array(3) { arrayOfNulls<TextView>(3) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,17 +26,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Получаем ссылки на все TextView, которые будут обновляться
-        textViews = listOf(
-            findViewById(R.id.textViewLinearHorizontal1),
-            findViewById(R.id.textViewLinearHorizontal2),
-            findViewById(R.id.textViewLinearHorizontal3),
-            findViewById(R.id.textViewLinearVertical1),
-            findViewById(R.id.textViewLinearVertical2),
-            findViewById(R.id.textViewLinearVertical3),
-            findViewById(R.id.textViewConstraint1),
-            findViewById(R.id.textViewConstraint2),
-            findViewById(R.id.textViewConstraint3)
-        )
+        textViews[0][0] = findViewById(R.id.textViewLinearHorizontal1);
+        textViews[0][1] = findViewById(R.id.textViewLinearHorizontal2);
+        textViews[0][2] = findViewById(R.id.textViewLinearHorizontal3);
+        textViews[1][0] = findViewById(R.id.textViewLinearVertical1);
+        textViews[1][1] = findViewById(R.id.textViewLinearVertical2);
+        textViews[1][2] = findViewById(R.id.textViewLinearVertical3);
+        textViews[2][0] = findViewById(R.id.textViewConstraint1);
+        textViews[2][1] = findViewById(R.id.textViewConstraint2);
+        textViews[2][2] = findViewById(R.id.textViewConstraint3);
 
         // Обработчик нажатия кнопки
         val button: Button = findViewById(R.id.button)
@@ -42,14 +43,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Метод для увеличения значения в TextView
     private fun incrementTextView() {
-        // Найдем следующий TextView, который нужно обновить
-        for (textView in textViews) {
-            if (textView.text.isEmpty()) {
-                textView.text = (++currentValue).toString()
-                return
+        val index = currentValue % 3
+
+        for (i1 in 0..2) {
+            val isEqual: Boolean = i1 == index
+            if (isEqual) {
+                for (i2 in 0..2) {
+                    textViews[i1][i2]?.text = "${index+1}"
+                }
+            } else {
+                for (i2 in 0..2) {
+                    textViews[i1][i2]?.text = " "
+                }
             }
         }
+        ++currentValue;
     }
 }
