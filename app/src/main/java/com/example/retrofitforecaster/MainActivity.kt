55512c75b8f439db0989b532f7d4dd283b83939d
Станислав Хозяенок <st.hoz.kem@gmail.com>
2024-12-05 19:14:35 +0700

    Исправлена ошибка сертификатов

--- app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
+++ app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
@@ -51,11 +51,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     object RetrofitHelper {
-        val baseUrl = "http://api.openweathermap.org/data/2.5/"
+        private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
         fun getInstance(): Retrofit {
-            return Retrofit.Builder().baseUrl(baseUrl)
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
     }
+
 }
