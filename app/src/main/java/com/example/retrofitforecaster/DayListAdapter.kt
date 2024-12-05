8d96bd1157feb21b047d5362d3b851a23122cd53
Станислав Хозяенок <st.hoz.kem@gmail.com>
2024-12-05 19:06:25 +0700

    start testing

---
+++ app/src/main/java/com/example/retrofitforecaster/DayListAdapter.kt
@@ -0,0 +1,77 @@
+package com.example.retrofitforecaster
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+const val VIEW_TYPE_HOT = 0;
+const val VIEW_TYPE_COLD = 1;
+
+class DayListAdapter : ListAdapter<DayPrognosis, RecyclerView.ViewHolder>(DayDiffCallback()) {
+
+    inner class DayViewHolderHot(view: View) : RecyclerView.ViewHolder(view) {
+        val datetime: TextView = view.findViewById(R.id.datetime)
+        val plusTxt: TextView = view.findViewById(R.id.txt_plus_temperature)
+        val icon: ImageView = view.findViewById(R.id.icon)
+
+        fun bind(day: DayPrognosis) {
+            datetime.text = day.dt_txt
+            plusTxt.text = day.main.getTempAsString()
+            val iconUrl = "https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+            Glide.with(icon).load(iconUrl).into(icon)
+        }
+    }
+
+    inner class DayViewHolderCold(view: View) : RecyclerView.ViewHolder(view) {
+        val datetime: TextView = view.findViewById(R.id.datetime)
+        val minusTxt: TextView = view.findViewById(R.id.txt_minus_temperature)
+        val icon: ImageView = view.findViewById(R.id.icon)
+
+        fun bind(day: DayPrognosis) {
+            datetime.text = day.dt_txt
+            minusTxt.text = day.main.getTempAsString()
+            val iconUrl = "https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+            Glide.with(icon).load(iconUrl).into(icon)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val day = currentList[position]
+        return if (day.main.temp > 0) VIEW_TYPE_HOT else VIEW_TYPE_COLD
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_HOT) {
+            DayViewHolderHot(LayoutInflater.from(parent.context).inflate(R.layout.r_item_hot, parent, false))
+        } else {
+            DayViewHolderCold(LayoutInflater.from(parent.context).inflate(R.layout.r_item_cold, parent, false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val day = currentList[position]
+        if (holder is DayViewHolderHot) {
+            holder.bind(day)
+        } else if (holder is DayViewHolderCold) {
+            holder.bind(day)
+        }
+    }
+
+    override fun getItemCount(): Int = currentList.size
+}
+
+class DayDiffCallback : DiffUtil.ItemCallback<DayPrognosis>() {
+    override fun areItemsTheSame(oldItem: DayPrognosis, newItem: DayPrognosis): Boolean {
+        return oldItem.dt_txt == newItem.dt_txt
+    }
+
+    override fun areContentsTheSame(oldItem: DayPrognosis, newItem: DayPrognosis): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
+++ app/src/main/java/com/example/retrofitforecaster/MainActivity.kt
@@ -5,6 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +25,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val rView: RecyclerView = findViewById(R.id.r_view)
+        rView.layoutManager = LinearLayoutManager(this)
+        val daysApi = RetrofitHelper.getInstance().create(DayGetter::class.java)
+
+        val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            throwable.printStackTrace()
+        }
+
+        GlobalScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
+            val days = daysApi.check(BuildConfig.API_KEY_OPEN_WEATHER_MAP)
+
+            withContext(Dispatchers.Main) {
+                if (days.body() != null) {
+                    val adapter = DayListAdapter()
+                    adapter.submitList(days.body()?.list)
+                    rView.adapter = adapter
+                }
+            }
+        }
+
+    }
+
+    object RetrofitHelper {
+        val baseUrl = "http://api.openweathermap.org/data/2.5/"
+        fun getInstance(): Retrofit {
+            return Retrofit.Builder().baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         }
     }
 }
